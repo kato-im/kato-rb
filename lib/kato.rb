9f9c3d2c973620d67b7ae472e90e0135f4e6e24d
JLarky <jlarky@gmail.com>
2014-05-23 18:18:37 +0600

    allow to post to integrations other than webhooks, also allow to specify different api_url

--- lib/kato.rb
+++ lib/kato.rb
@@ -7,9 +7,15 @@ module Kato
   class Room
     include HTTParty
 
-    def initialize(room_id)
-      @room_id = room_id
-      self.class.base_uri 'https://api.kato.im/rooms/' + @room_id
+    def initialize(room_id, options = {})
+      default_options = {
+        :api_url => 'https://api.kato.im',
+        :integration => 'simple'
+      }
+      options = default_options.merge options
+      url = options[:api_url] + '/rooms/' + room_id + '/' + options[:integration]
+      url = options[:full_url] if options[:full_url]
+      self.class.base_uri url
     end
 
     def post(from, message, options = {})
@@ -17,7 +23,7 @@ module Kato
         :from => from,
         :text => message
       }.merge options
-      response = self.class.post('/simple',
+      response = self.class.post('',
         :headers => { 'Content-Type' => 'application/json' },
         :body => body.to_json )
       case response.code
